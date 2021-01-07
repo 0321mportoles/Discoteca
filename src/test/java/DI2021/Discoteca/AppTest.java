36f59a299319230820856c10c1130b2b322d02a0
marta <0321mportoles@e-itaca.es>
2021-01-07 18:38:23 +0100

    test de la funcion

--- src/test/java/DI2021/Discoteca/AppTest.java
+++ src/test/java/DI2021/Discoteca/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import DI2021.Discoteca.utils.AlbumUtils;
+
 /**
  * Unit test for simple App.
  */
@@ -16,5 +18,17 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    
+    public void shouldBeValid() {
+    	String metaInfo = null;
+		AlbumUtils.ValidarURLMetadata(metaInfo);
+    	assertTrue("La direccion es correcta", true);
+    }
+    
+    public void shouldBeInvalid() {
+    	String metaInfo = null;
+		AlbumUtils.ValidarURLMetadata(metaInfo);
+    	assertTrue("La direccion es incorrecta", false);
     }
 }
